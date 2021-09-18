d1bad6e0e93a9ad5c1ef72f95d5a4e93806411af
Binay bk <khadkabinay@yahoo.com>
2021-09-18 00:25:59 -0500

    Updates header.css

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,66 +1,78 @@
-import React from 'react';
-import {useEffect} from 'react'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import UserModel from "../../models/UserModel";
-import { useRecoilState } from "recoil"
+import { useRecoilState } from "recoil";
 import { userState } from "../../recoil/atoms";
-import './Header.css';
-
-
-
+import "./Header.css";
 
 const Header = (props) => {
-    const [user, setUser] = useRecoilState(userState)
+  const [user, setUser] = useRecoilState(userState);
 
-    
   useEffect(function () {
     if (localStorage.getItem("uid")) {
       UserModel.all().then((response) => {
-        setUser(response.data)
-      })
+        setUser(response.data);
+      });
     }
-
   }, []);
 
-
-  function logOut(){
-      setUser(null)
-      localStorage.clear()
+  function logOut() {
+    setUser(null);
+    localStorage.clear();
   }
 
-
-    return (
-        <header >
-            <>
-            {user ? (
-              <div >
-              {user.role === "admin" ? (<div>
-                      <ul className="header">
-                          <li><NavLink to='/users' className="btn btn-style">Admin</NavLink></li>    
-                          <li><NavLink to='/' onClick={logOut} className="btn btn-style">Log Out</NavLink></li>    
-                      </ul> 
-                      </div>) : (<div>
-                      <ul className="header">
-                            <li><NavLink to={`/users/${user._id}`} className="btn btn-style">Employee</NavLink></li> 
-                            <li><NavLink to='/' onClick={logOut} className="btn btn-style">Log Out</NavLink></li> 
-                      </ul>
-                
-                    </div>)}
-                </div> ) :(
-                <div >
-                    <ul className="header">
-                        <li><NavLink to='/' className="btn btn-style">Home</NavLink></li>
-                        <li><NavLink to={"/login"} className="btn btn-style">Login</NavLink> </li>
-                        <li><NavLink to='/register' className="btn btn-style">Sign Up</NavLink></li>
-                    </ul>
-                </div>
-                ) }
-
-            </>
-        </header>
-    )
-}
+  return (
+    <header>
+      <>
+        {user ? (
+          <div>
+            {user.role === "admin" ? (
+              <div>
+                <ul className="header">
+                  <li>
+                    <NavLink to="/users">Admin</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/" onClick={logOut}>
+                      Log Out
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <div>
+                <ul className="header">
+                  <li>
+                    <NavLink to={`/users/${user._id}`}>Employee</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/" onClick={logOut}>
+                      Log Out
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        ) : (
+          <div>
+            <ul className="header">
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/login"}>Login</NavLink>{" "}
+              </li>
+              <li>
+                <NavLink to="/register">Sign Up</NavLink>
+              </li>
+            </ul>
+          </div>
+        )}
+      </>
+    </header>
+  );
+};
 
 export default Header;
-
-

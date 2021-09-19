1c5e7bdb91611cde40999ad136851bea78a35b3a
Binay bk <khadkabinay@yahoo.com>
2021-09-18 23:53:53 -0500

    Updates User.js

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,46 +1,43 @@
 const mongoose = require("mongoose");
 
-
 // SETS UP SCHEMA
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: [true, "You must provide a name."] },
-    username: {type: String, required: true, unique: true},
+    username: { type: String, required: true, unique: true },
     email: {
       type: String,
       required: true,
       unique: true,
-      match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+      match:
+        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
     },
-    image: {type: String, required:[true, "You must provide a image."] },
-    password: {type: String, required: true},
-    phoneNumber: {type: Number, required:[true, "You must provide a phone number."] },
-    completedTask:{type: Number, default: 0},
-     role :{type: String ,default:"admin"},
-    
-    // REFERENCE TO TASKS
-    tasks: [ { type: mongoose.Schema.Types.ObjectId, ref: "Task" } ]
- },
-  {
-    timestamps: true, 
+    image: { type: String, required: [true, "You must provide a image."] },
+    password: { type: String, required: true },
+    phoneNumber: {
+      type: Number,
+      required: [true, "You must provide a phone number."],
+    },
+    completedTask: { type: Number, default: 0 },
+    role: { type: String, default: "admin" },
 
+    // REFERENCE TO TASKS
+    tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
+  },
+  {
+    timestamps: true,
   }
 );
 
-
+// DELETES PASSWORD'S FIELD
 userSchema.set("toJSON", {
-transform: (doc, ret, opt) => {
-  delete ret["password"];
-  return ret;
-},
+  transform: (doc, ret, opt) => {
+    delete ret["password"];
+    return ret;
+  },
 });
 
-
-
-
-// CREATES A MODEL 
+// CREATES A MODEL
 const User = mongoose.model("User", userSchema);
-
-
 
 module.exports = User;

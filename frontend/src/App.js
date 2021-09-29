f64e32ed19d9fbeed0feabe5a601912a060b6893
Binay bk <khadkabinay@yahoo.com>
2021-09-29 16:37:36 -0500

    Updates App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,13 @@
-import React from 'react';
-import Routes from './config/Routes';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import classes from './App.module.css'
-
-
-
+import React from "react";
+import Routes from "./config/Routes";
+import Header from "./components/Header/Header";
+import classes from "./App.module.css";
 
 function App() {
   return (
     <div className={`${classes.App}`}>
-        <Header />
-        <Routes/>
-
+      <Header />
+      <Routes />
     </div>
   );
 }

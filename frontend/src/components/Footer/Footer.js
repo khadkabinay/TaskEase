19039a341dc2ede19bce09ed4df9cf69627142b6
Binay bk <khadkabinay@yahoo.com>
2021-10-18 22:10:51 -0500

    Updates Footer.js component

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import classes from './Footer.module.css'
-
-
+import React from "react";
+import classes from "./Footer.module.css";
 
 const Footer = (props) => {
-    return (
-        <footer >
-            <div className={classes.FooterBar}>
-                <h5>Copyright@TaskEase2020</h5>
-                
-            </div>
-        
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <div className={classes.FooterBar}>
+        <h5>Copyright@TaskEase2020</h5>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
